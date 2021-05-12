7eceea73bcc5e272f25a7f90a2f5d4071d4bedc7
chaiyd <it016@antpkg.com>
2021-05-12 16:49:08 +0800

    kubernetes containerd kubeadm

--- kubernetes/scp.sh
+++ kubernetes/scp.sh
@@ -4,14 +4,10 @@ USER=root
 CONTROL_PLANE_IPS="192.168.10.10 192.168.10.11"
 
 for host in ${CONTROL_PLANE_IPS}; do
-    scp /etc/kubernetes/pki/ca.crt "${USER}"@$host:/etc/kubernetes/pki/
-    scp /etc/kubernetes/pki/ca.key "${USER}"@$host:/etc/kubernetes/pki/
-    scp /etc/kubernetes/pki/sa.key "${USER}"@$host:/etc/kubernetes/pki/
-    scp /etc/kubernetes/pki/sa.pub "${USER}"@$host:/etc/kubernetes/pki/
-    scp /etc/kubernetes/pki/front-proxy-ca.crt "${USER}"@$host:/etc/kubernetes/pki/
-    scp /etc/kubernetes/pki/front-proxy-ca.key "${USER}"@$host:/etc/kubernetes/pki/
-    scp /etc/kubernetes/pki/etcd/ca.crt "${USER}"@$host:/etc/kubernetes/pki/etcd/
-    scp /etc/kubernetes/pki/etcd/ca.key "${USER}"@$host:/etc/kubernetes/pki/etcd/
-    scp /etc/kubernetes/pki/apiserver-etcd-client.crt "${USER}"@$host:/etc/kubernetes/pki/
-    scp /etc/kubernetes/pki/apiserver-etcd-client.key "${USER}"@$host:/etc/kubernetes/pki/
+    ssh "${USER}"@$host "mkdir -p /etc/kubernetes/pki/etcd"
+    scp /etc/kubernetes/pki/ca.* "${USER}"@$host:/etc/kubernetes/pki/
+    scp /etc/kubernetes/pki/sa.* "${USER}"@$host:/etc/kubernetes/pki/
+    scp /etc/kubernetes/pki/front-proxy-ca.* "${USER}"@$host:/etc/kubernetes/pki/
+    scp /etc/kubernetes/pki/etcd/ca.* "${USER}"@$host:/etc/kubernetes/pki/etcd/
+    scp /etc/kubernetes/admin.conf "${USER}"@$host:/etc/kubernetes/
 done
